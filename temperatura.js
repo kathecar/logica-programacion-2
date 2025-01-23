c84fdb5667299a17ae8b3b4340992f8f8c50958d
kathecar <katherinnecardenas16@gmail.com>
2025-01-22 23:25:59 -0500

    Corrijo nuevamente el codigo, ahora sale alerta

--- temperatura.js
+++ temperatura.js
@@ -1,21 +1,47 @@
-const prompt = require("prompt-sync")();  //Se importa el paquete promp-sync
+//const prompt = require("prompt-sync")();  //Se importa el paquete promp-sync
 
 // la temperatura en grados Celsius
 
-function convertirTemperatura() {
-const temperatura = Number(prompt("Escriba la temperatura en grados Celsius: ")); 
-document.querySelector('#fahrenheit input').value =("La temperatura en grados Fahrenheit es: ", ((temperatura * 9) / 5) + 32); //temperatura en grados Fahrenheit
-document.querySelector('#Kelvin input').value =("La temperatura en grados Kelvin es: ", temperatura + 273.15); //temperatura en grados Kelvin
-if (isNaN(temperatura)) {
-    console.log("Por favor, ingrese un número que sea valido");
-}
-}
+// function convertirTemperatura() {
+// const temperatura = Number(prompt("Escriba la temperatura en grados Celsius: ")); 
+// document.querySelector('#fahrenheit input').value =("La temperatura en grados Fahrenheit es: ", ((temperatura * 9) / 5) + 32); //temperatura en grados Fahrenheit
+// document.querySelector('#Kelvin input').value =("La temperatura en grados Kelvin es: ", temperatura + 273.15); //temperatura en grados Kelvin
 
-function limpiarCampos() {
-    document.querySelector('#celsius input').value = '';
-    document.querySelector('#fahrenheit input').value = '';
-    document.querySelector('#Kelvin input').value = '';
-}
+// if (isNaN(temperatura)) {
+//     console.log("Por favor, ingrese un número que sea valido");
+// } else {
+//     const temperatura = Number(prompt("Escriba la temperatura en grados Celsius: ")); 
+//     document.querySelector('#fahrenheit input').value =("La temperatura en grados Fahrenheit es: ", ((temperatura * 9) / 5) + 32); //temperatura en grados Fahrenheit
+//     document.querySelector('#Kelvin input').value =("La temperatura en grados Kelvin es: ", temperatura + 273.15); //temperatura en grados Kelvin
+// }    
+// function limpiarCampos() {
+//     document.querySelector('#celsius input').value = '';
+//     document.querySelector('#fahrenheit input').value = '';
+//     document.querySelector('#Kelvin input').value = '';
+// }
 
-document.getElementById("convertir").addEventListener("click", convertirTemperatura);
-document.getElementById("limpiar").addEventListener("click", limpiarCampos);
+// document.getElementById("convertir").addEventListener("click", convertirTemperatura);
+// document.getElementById("limpiar").addEventListener("click", limpiarCampos);
+// }
+
+let botonConvertir = document.getElementById("convertir");
+let botonLimpiar = document.getElementById("limpiar");
+
+botonConvertir.addEventListener("click", function () {
+    let celsius = parseFloat(document.getElementById("celsius").value);
+  
+    if (isNaN(celsius)) { 
+        alert("Por favor, ingrese un número válido."); 
+        return; 
+      }
+    let fahrenheit = ((celsius * 9) / 5) + 32;
+    let kelvin = celsius + 273.15;
+
+    document.getElementById("fahrenheit").value = fahrenheit;
+    document.getElementById("Kelvin").value = kelvin;
+});
+botonLimpiar.addEventListener("click", function() {
+    document.getElementById("celsius").value = '';
+    document.getElementById("fahrenheit").value = '';
+    document.getElementById("Kelvin").value = '';
+});
