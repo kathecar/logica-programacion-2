6ad0e2ed4f7d9e3da8de41f99594f18dee1a5e43
kathecar <katherinnecardenas16@gmail.com>
2025-01-22 22:44:37 -0500

    Agrego la parte de diseño

--- temperatura.js
+++ temperatura.js
@@ -1,10 +1,21 @@
 const prompt = require("prompt-sync")();  //Se importa el paquete promp-sync
 
 // la temperatura en grados Celsius
+
+function convertirTemperatura() {
 const temperatura = Number(prompt("Escriba la temperatura en grados Celsius: ")); 
-console.log("La temperatura en grados Fahrenheit es: ", ((temperatura * 9) / 5) + 32); //temperatura en grados Fahrenheit
-console.log("La temperatura en grados Kelvin es: ", temperatura + 273.15); //temperatura en grados Kelvin
+document.querySelector('#fahrenheit input').value =("La temperatura en grados Fahrenheit es: ", ((temperatura * 9) / 5) + 32); //temperatura en grados Fahrenheit
+document.querySelector('#Kelvin input').value =("La temperatura en grados Kelvin es: ", temperatura + 273.15); //temperatura en grados Kelvin
 if (isNaN(temperatura)) {
     console.log("Por favor, ingrese un número que sea valido");
 }
+}
 
+function limpiarCampos() {
+    document.querySelector('#celsius input').value = '';
+    document.querySelector('#fahrenheit input').value = '';
+    document.querySelector('#Kelvin input').value = '';
+}
+
+document.getElementById("convertir").addEventListener("click", convertirTemperatura);
+document.getElementById("limpiar").addEventListener("click", limpiarCampos);
